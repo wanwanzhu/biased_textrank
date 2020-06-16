reduced computation for biased_summarization.py + bugfix

src/biased_summarization.py
@@ -46,7 +46,7 @@ def biased_textrank(texts_embeddings, bias_embedding, damping_factor=0.80, simil
 
     print('Calculating ranks...')
     ranks = np.ones((len(matrix), 1)) / len(matrix)
-    iterations = 120
+    iterations = 40
     for i in range(iterations):
         ranks = scaled_matrix.T.dot(ranks)
 
@@ -106,20 +106,19 @@ def main():
     republican_summaries = [{'filename': filename} for filename in get_filenames_in_directory(republican_path)]
     normal_summaries = [{'filename': filename} for filename in get_filenames_in_directory(transcript_path)]
     for i, transcript in enumerate(transcripts):
+        if len(democrat_gold_standards[i]['content']) == 0 and len(republican_gold_standards[i]['content']) == 0:
+            continue
         transcript_sentences = get_sentences(transcript['content'])
         transcript_sentence_embeddings = [get_sbert_embedding(sentence) for sentence in transcript_sentences]
-        if len(democrat_gold_standards[i]['content']) != 0:
-            democratic_ranks = biased_textrank(transcript_sentence_embeddings, democratic_bias_embedding)
-            democrat_summary = ' '.join(select_top_k_texts_preserving_order(transcript_sentences, democratic_ranks, 20))
-            democrat_summaries[i]['content'] = democrat_summary
-        if len(republican_gold_standards[i]['content']) != 0:
-            republican_ranks = biased_textrank(transcript_sentence_embeddings, republican_bias_embedding)
-            republican_summary = ' '.join(select_top_k_texts_preserving_order(transcript_sentences, republican_ranks, 20))
-            republican_summaries[i]['content'] = republican_summary
-        if len(republican_gold_standards[i]['content']) != 0 or len(democrat_gold_standards[i]['content']) != 0:
-            normal_ranks = biased_textrank(transcript_sentence_embeddings, None, damping_factor=0.85, biased=False)
-            normal_summary = ' '.join(select_top_k_texts_preserving_order(transcript_sentences, normal_ranks, 20))
-            normal_summaries[i]['content'] = normal_summary
+        democratic_ranks = biased_textrank(transcript_sentence_embeddings, democratic_bias_embedding)
+        democrat_summary = ' '.join(select_top_k_texts_preserving_order(transcript_sentences, democratic_ranks, 20))
+        democrat_summaries[i]['content'] = democrat_summary
+        republican_ranks = biased_textrank(transcript_sentence_embeddings, republican_bias_embedding)
+        republican_summary = ' '.join(select_top_k_texts_preserving_order(transcript_sentences, republican_ranks, 20))
+        republican_summaries[i]['content'] = republican_summary
+        normal_ranks = biased_textrank(transcript_sentence_embeddings, None, damping_factor=0.85, biased=False)
+        normal_summary = ' '.join(select_top_k_texts_preserving_order(transcript_sentences, normal_ranks, 20))
+        normal_summaries[i]['content'] = normal_summary
 
     # saving results
     with open('democrat_summaries.json', 'w') as f:
@@ -130,10 +129,12 @@ def main():
         f.write(json.dumps(normal_summaries))
 
     # load results
-    # with open('democrat_summaries.json') as f:
+    # with open('../democrat_summaries.json') as f:
     #     democrat_summaries = json.load(f)
-    # with open('republican_summaries.json') as f:
+    # with open('../republican_summaries.json') as f:
     #     republican_summaries = json.load(f)
+    # with open('../normal_summaries.json') as f:
+    #     normal_summaries = json.load(f)
 
     # evaluation
     democrat_rouge_scores = calculate_rouge_score(democrat_gold_standards, democrat_summaries)