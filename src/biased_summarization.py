bd25dc6430d138588f9d55de552284c89f17ad72
ashkank <ashkank@umich.edu>
2020-06-30 13:06:01 -0400

    final parameter setup for presenting ablation study results

--- src/biased_summarization.py
+++ src/biased_summarization.py
@@ -257,7 +257,7 @@ def ablation_study():
     for item in sentences_and_embeddings:
         item['embeddings'] = np.array(item['embeddings'])
 
-    damping_factors = [0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9]
+    damping_factors = [0.8, 0.85, 0.9]
     similarity_thresholds = [0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9]
 
     democratic_summaries = [{'filename': item['filename']} for item in sentences_and_embeddings]
